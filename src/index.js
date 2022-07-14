88a2c46da8f9ccd6b34500c472ff2c668384527e
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-07-14 18:51:47 -0500

    import things needed for redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//? create a store using combine reducers
+const store = createStore(
+	combineReducers({
+		thing1,
+		thing2,
+		thing3,
+		thing4,
+	})
+);
+
+//? add provider
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
 registerServiceWorker();
