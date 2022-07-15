4591d9a5be8da06dd5f164353dda06023cb1ca06
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-07-14 20:48:23 -0500

    finish post route

--- server/routes/feedback-router.js
+++ server/routes/feedback-router.js
@@ -4,10 +4,32 @@ const pool = require('../modules/pool.js');
 
 router.get('/', (req, res) => {
 	console.log('get');
+	const sqlString = 'SELECT * FROM feedback;';
+	pool
+		.query(sqlString)
+		.then((result) => {
+			res.sendStatus(201);
+		})
+		.catch((err) => {
+			console.log('Error in post', err);
+			res.sendStatus(500);
+		});
 });
 
 router.post('/', (req, res) => {
 	console.log('get');
+	const sqlString =
+		'INSERT INTO feedback (feeling, understanding, support, comments) VALUES ($1, $2, $3, $4);';
+	const { feeling, understanding, support, comments } = req.body;
+	pool
+		.query(sqlString, [feeling, understanding, support, comments])
+		.then((result) => {
+			res.sendStatus(201);
+		})
+		.catch((err) => {
+			console.log('Error in post', err);
+			res.sendStatus(500);
+		});
 });
 
 module.exports = router;
